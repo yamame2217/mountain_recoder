89dea34367ae523150226bd927741f229b42f689
yamame <78255564+yamame2217@users.noreply.github.com>
2025-08-25 16:40:20 +0900

    詳細ページとテストデータの登録

--- paplib/urls.py
+++ paplib/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.mountain_list, name='mountain_list'),
+    path('mountain/<int:pk>/', views.mountain_detail, name='mountain_detail'),
 ]
--- paplib/views.py
+++ paplib/views.py
@@ -4,4 +4,9 @@ from .models import Mountain
 def mountain_list(request):
     mountains = Mountain.objects.all()
     context = {'mountains': mountains}
-    return render(request, 'paplib/mountain_list.html', context) 
+    return render(request, 'paplib/mountain_list.html', context)
+
+def mountain_detail(request, pk):
+    mountain = Mountain.objects.get(pk=pk) 
+    context = {'mountain': mountain}
+    return render(request, 'paplib/mountain_detail.html', context)
