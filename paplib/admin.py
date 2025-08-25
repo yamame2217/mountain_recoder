3898b2058baa9f04d03e912c22eef4c0f510e1fd
yamame <78255564+yamame2217@users.noreply.github.com>
2025-08-25 16:23:05 +0900

    model, view, templateの最低限の機能の実装

--- paplib/admin.py
+++ paplib/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Mountain, ClimbRecord  
 
 # Register your models here.
+admin.site.register(Mountain)      
+admin.site.register(ClimbRecord)   
--- paplib/views.py
+++ paplib/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Mountain  
 
-# Create your views here.
+def mountain_list(request):
+    mountains = Mountain.objects.all()
+    context = {'mountains': mountains}
+    return render(request, 'paplib/mountain_list.html', context) 
